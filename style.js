65622246e3518d53d812220eba2d0d24c25c5416
Tony Jeffries <trjeffries4@cps.edu>
2025-03-13 14:39:18 +0000

    first commit

---
+++ style.js
@@ -0,0 +1,17 @@
+console.log("work")
+
+let food = document.querySelector(".food");
+console.log(food)
+let color = document.querySelector(".color");
+console.log(color)
+let button = document.querySelector("button");
+console.log(button)
+
+button.onclick = function(){
+}
+let answerfood = food.value;
+let answercolor = color.value;
+
+if(answerfood === "Chili Dog"){
+    inner.HTML
+}
